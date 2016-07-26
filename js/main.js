5d39dbe2a0e143968f524ecaf9b6dc587d3d0742
WebberWu <bird23074035@gmail.com>
2016-07-27 03:01:55 +0800

    小女孩的 mask

--- js/main.js
+++ js/main.js
@@ -1,11 +1,22 @@
 $(function() {
-    // $('.title-page').
-    var hoverSrc, originalSrc;
-    $('.title-page').hover(function() {
-        originalSrc = $(this).find('img').attr('src');
-        hoverSrc = originalSrc.replace('title-page', 'title-page-mask');
-        $(this).find('img').attr('src', hoverSrc);
-    }, function() {
-        $(this).find('img').attr('src', originalSrc);
-    });
+    var hoverAction = function(event, theString, replaceString){
+        var hoverSrc, originalSrc;
+        event.hover(function() {
+            originalSrc = $(this).find('img').attr('src');
+            hoverSrc = originalSrc.replace(theString, replaceString);
+            $(this).find('img').attr('src', hoverSrc);
+        }, function() {
+            $(this).find('img').attr('src', originalSrc);
+        });
+      };
+    hoverAction($('.title-page'), 'title-page', 'title-page-mask');
+    hoverAction($('.product-box'), '-img.png', '-img-mask.png');
+    // var hoverSrc, originalSrc;
+    // $('.title-page').hover(function() {
+    //     originalSrc = $(this).find('img').attr('src');
+    //     hoverSrc = originalSrc.replace('title-page', 'title-page-mask');
+    //     $(this).find('img').attr('src', hoverSrc);
+    // }, function() {
+    //     $(this).find('img').attr('src', originalSrc);
+    // });
 });
